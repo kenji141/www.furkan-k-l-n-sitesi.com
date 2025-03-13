da14a5b81eb0daa8489c6faf80c71686044f9ea4
furkan kılıç <hatice2727ws@gmail.com>
2025-03-13 22:52:24 +0300

    Add files via upload

---
+++ web sayfası/script.js
@@ -0,0 +1,4 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Sayfanın yenilenmesini engelle
+    alert("Mesajınız başarıyla gönderildi!");
+});
